66644e7833effc125a3a8ccbaeaa8cfe51414020
iescheinberg <agr.scheinberg@gmail.com>
2024-02-22 15:02:16 -0300

    mejorando funciones

--- js/index.js
+++ js/index.js
@@ -6,19 +6,35 @@
 */
 
 // 1----------------------------------------------------------------------------------------------------------------------------------
-/* function caractersCount(param) {
-    return String(param).length;
+/* function caractersCount(param = "") {
+    if (!param) {
+        console.warn("No enviaste ninguna cadena de caracteres")
+    } else {
+        return String(param).length;
+    }
 }
+console.log(caractersCount(15)); */
 
-console.log(caractersCount("Hola Mundo")); */
 
+
+/* const caractersCount = (param = "") => !param ? console.warn("No ingresaste una cadena") : console.log(String(param).length)
+
+
+caractersCount("Hello") */
 
 // 2----------------------------------------------------------------------------------------------------------------------------------
 /* function cutText(text, num) {
     return String(text).slice(0, num);
 }
-
 console.log(cutText("Hola ", 2)); */
+
+
+
+/* const cutText = (text = "", num) => !text
+    ? console.warn("No ingresaste una cadena")
+    : console.log(String(text).slice(0, num))
+
+cutText("Hola mundo", 4) */
 
 
 // 3----------------------------------------------------------------------------------------------------------------------------------
@@ -29,6 +45,12 @@ console.log(cutText("Hola ", 2)); */
 console.log(splitString("hola como estas?", " ")) */
 
 
+/* const splitString = (text = "", simbol) => !text
+    ? console.warn("No ingresaste una cadena")
+    : console.log(String(text).split(simbol))
+
+splitString("Hola Mundo", " ")
+ */
 // 4----------------------------------------------------------------------------------------------------------------------------------
 /* function repeatText(text, repeat) {
     let txt = String(text);
@@ -38,3 +60,24 @@ console.log(splitString("hola como estas?", " ")) */
 }
 
 repeatText('console', 5) */
+
+
+
+const repeatText = (text = "", repeat = undefined) => {
+    if (!text) return console.warn("No ingresaste una cadena")
+
+    if (repeat === undefined) return console.warn("No ingresaste un numero")
+
+    if (repeat === 0) return console.warn("El valor ingresado no puede ser 0")
+
+    if (Math.sign(repeat) === -1) return console.warn("No puedes ingresar un número negativo")
+
+    for (let i = 0; i < repeat; i++) {
+        console.log(String(text));
+    }
+}
+
+repeatText("Hola Mundo", 3)
+
+
+// 5----------------------------------------------------------------------------------------------------------------------------------
