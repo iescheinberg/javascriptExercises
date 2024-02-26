22e98f3351654a325875b6a91e7ebdd32ac2c410
iescheinberg <agr.scheinberg@gmail.com>
2024-02-26 17:52:22 -0300

    ultimo

--- js/index.js
+++ js/index.js
@@ -1041,7 +1041,7 @@ const promedio = (arr = undefined) => {
     )
 }
 
-promedio([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+
 
 
 
@@ -1074,4 +1074,169 @@ promedio([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
 */
 // ------------------------------------------------------------------------------------------------------------------------------------
 
+class Pelicula {
+    constructor({ id, titulo, director, estreno, pais, generos, calificacion }) {
+        this.id = id;
+        this.titulo = titulo;
+        this.director = director;
+        this.estreno = estreno;
+        this.pais = pais;
+        this.genero = generos;
+        this.calificacion = calificacion;
 
+        this.validarIMBD(id);
+        this.validarTitulo(titulo);
+        this.validarDirector(director);
+        this.validarEstreno(estreno);
+        this.validarPais(pais);
+        this.validarGeneros(generos);
+        this.validarCalificacion(calificacion)
+    }
+
+    static get listaGeneros() {
+        return ['Action', 'Adult', 'Adventure', 'Animation', 'Biography', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Family', 'Fantasy', 'Film Noir', 'Game-Show', 'History', 'Horror', 'Musical', 'Music', 'Mystery', 'News', 'Reality-TV', 'Romance', 'Sci-Fi', 'Short', 'Sport', 'Talk-Show', 'Thriller', 'War', 'Western']
+    }
+
+    static generosAceptados() {
+        return console.info(`Los géneros aceptados son : ${Pelicula.listaGeneros.join(", ")}`)
+    }
+
+    validarCadena(propiedad, valor) {
+        if (!valor) return console.warn(`${propiedad} "${valor} está vacío"`)
+
+        if (typeof valor !== 'string') return console.error(`${propiedad} "${valor} ingresado, NO es una cadena de texto.`);
+
+        return true
+    }
+
+    validarLongitudCadena(propiedad, valor, longitud) {
+        if (valor.length > longitud) return console.error(`${propiedad} "${valor}" excede el número de caracteres permitidos (${longitud}).`)
+
+        return true
+    }
+
+    validarNumero(propiedad, valor) {
+        if (!valor) return console.warn(`${propiedad} "${valor} está vacío"`);
+        if (typeof valor !== 'number') return console.error(`${propiedad} "${valor}" ingresado, No es un número`)
+
+        return true;
+    }
+
+    validarArreglo(propiedad, valor) {
+        if (!valor) return console.warn(`${propiedad} "${valor}", está vacío`);
+        if (!(valor instanceof Array)) return console.error(`${propiedad} "${valor}", NO es un arreglo`);
+        if (valor.length === 0) return console.warn(`${propiedad} "${valor}", no tiene datos`)
+
+        for (const cadena of valor) {
+            if (typeof cadena !== "string") {
+                return console.error(`El valor ${cadena}, NO es un a cadena de texto`)
+            }
+        }
+
+        return true
+    }
+
+
+    validarIMBD(id) {
+        if (this.validarCadena("IMBD id", id)) {
+            if (!(/^([a-z]){2}([0-9]){7}$/.test(id))) {
+                return console.error(`IMBD id ${id} no es valido, debe tener 9 caracteres, los 2 primeros letras minúsculas, los restantes 7 son numeros`)
+            }
+        }
+    }
+
+    validarTitulo(titulo) {
+        if (this.validarCadena("Titulo", titulo)) {
+            this.validarLongitudCadena("Titulo", titulo, 100);
+        }
+    }
+    validarDirector(director) {
+        if (this.validarCadena("Director", director)) {
+            this.validarLongitudCadena("Director", director, 50);
+        }
+    }
+
+    validarEstreno(estreno) {
+        if (this.validarNumero("Año de Estreno", estreno)) {
+            if (!(/^([0-9]){4}$/.test(estreno))) {
+                return console.error(`Año de Estreno ${estreno} no es valido, debe ser un número de 4 dígitos.`)
+            }
+        }
+    }
+
+    validarPais(pais) {
+        this.validarArreglo("País", pais);
+    }
+
+    validarGeneros(generos) {
+        if (this.validarArreglo("Géneros", generos)) {
+            for (let genero of generos) {
+                if (!Pelicula.listaGeneros.includes(genero)) {
+                    console.error(`Gnénero(s) incorrectos "${generos.join(", ")}"`);
+                    Pelicula.generosAceptados()
+                }
+            }
+        }
+    }
+
+    validarCalificacion(calificacion) {
+        if (this.validarNumero("Calificación", calificacion))
+            return (calificacion < 0 || calificacion > 10)
+                ? console.error('La calificación debe estar en un rango de 0 y 10')
+                : this.calificacion = calificacion.toFixed(1)
+    }
+
+
+    fichaTecnica() {
+        console.info(`Ficha Técnica:\nTítulo: ${this.titulo}\nDirector: ${this.director}\nAño de Estreno: ${this.estreno}\nPaís: ${this.pais}\nGéneros:${this.genero}\nCalificación: ${this.calificacion}\nIMBD Id: ${this.id}`)
+    }
+}
+
+
+
+
+
+/* const peli = new Pelicula({
+    id: 'tt1234567',
+    titulo: "Título de la Peli",
+    director: "Director de la Peli",
+    estreno: 2020,
+    pais: ["México", "Francia"],
+    generos: ["War"],
+    calificacion: 7.789,
+})
+
+peli.fichaTecnica() */
+
+
+const misPelis = [
+    {
+        id: "tt0758758",
+        titulo: "Into the Wild",
+        director: "Sean Penn",
+        estreno: 2007,
+        pais: ["USA"],
+        generos: ["Adventure", "Biography", "Drama"],
+        calificacion: 8.1
+    },
+    {
+        id: "tt0479143",
+        titulo: "Rocky Balboa",
+        director: "Sylvester Stallone",
+        estreno: 2006,
+        pais: ["USA"],
+        generos: ["Action", "Drama", "Sport"],
+        calificacion: 7.1
+    },
+    {
+        id: "tt0468569",
+        titulo: "The Dark Knight",
+        director: "Christopher Nolan",
+        estreno: 2008,
+        pais: ["USA", "UK"],
+        generos: ["Action", "Crime", "Drama"],
+        calificacion: 9
+    }
+];
+
+misPelis.forEach(el => new Pelicula(el).fichaTecnica())
